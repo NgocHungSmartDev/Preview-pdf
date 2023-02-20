7b7320482df256c5f68c3aa5f98b3a62e3a968d7
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-20 21:56:23 +0700

    init BE

---
+++ PreviewPdf/src/main/java/com/ngochung/PreviewPdf/PreviewPdfApplication.java
@@ -0,0 +1,13 @@
+package com.ngochung.PreviewPdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreviewPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreviewPdfApplication.class, args);
+	}
+
+}
---
+++ PreviewPdf/src/test/java/com/ngochung/PreviewPdf/PreviewPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngochung.PreviewPdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreviewPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
