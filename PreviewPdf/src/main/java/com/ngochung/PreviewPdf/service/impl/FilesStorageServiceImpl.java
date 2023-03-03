a1a6408f617582dc3ea6617c5c941e9326db8e2e
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2023-03-03 20:30:23 +0700

    FE: init project process bar upload

--- PreviewPdf/src/main/java/com/ngochung/PreviewPdf/service/impl/FilesStorageServiceImpl.java
+++ PreviewPdf/src/main/java/com/ngochung/PreviewPdf/service/impl/FilesStorageServiceImpl.java
@@ -10,11 +10,13 @@ import java.util.stream.Stream;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ngochung.PreviewPdf.service.FilesStorageService;
 
+@Service
 public class FilesStorageServiceImpl implements FilesStorageService {
 
 	private final Path root = Paths.get("D:\\code practice\\Preview-pdf\\upload\\");
